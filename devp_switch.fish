703c67b72aee4ac6ea9751ef8dba437fe5dce6e2
Gabor Pihaj <gabor.pihaj@gmail.com>
2014-11-04 15:35:02 +0100

    Add unset functionality

--- devp_switch.fish
+++ devp_switch.fish
@@ -2,34 +2,50 @@ function devp_switch
 	
 	set -l DEV_HOME ~/dev
 
-
-	if test (count $argv) -eq 0
-		echo "Current project is: $DEV_PROJECT"
-
+	function unset_project
+		set -l DEV_HOME ~/dev
 		if test -n "$DEV_PROJECT"
-			cd $DEV_HOME/$DEV_PROJECT
-			fig ps
-		end
-	else if test (count $argv) -eq 1
-		cd ~/dev/$argv[1] 2>/dev/null
-
-		if test $status = 0
-			if test -n "$DEV_PROJECT"
-				echo "Stopping current project's containers ($DEV_PROJECT):"
-				echo ""
-				cd $DEV_HOME/$DEV_PROJECT
-				fig stop
-			end
-
-			set -Ux DEV_PROJECT $argv[1]
-
-			echo "Starting new project's containers ($DEV_PROJECT):"
+			echo "Stopping current project's containers ($DEV_PROJECT):"
 			echo ""
 			cd $DEV_HOME/$DEV_PROJECT
-			fig start
+			fig stop
+		end
+
+		set -Ux DEV_PROJECT 
+	end
+
+
+	if test (count $argv) -eq 0
+
+		if test -n "$DEV_PROJECT"
+			echo "Current project is: $DEV_PROJECT"
+
+			cd $DEV_HOME/$DEV_PROJECT
+			fig ps
 		else 
-			echo "Invalid project name"
-		end 
+			echo "There isn't running project"
+		end
+	else if test (count $argv) -eq 1
+		if test $argv[1] = "--unset"
+			unset_project
+		else 
+
+			cd ~/dev/$argv[1] 2>/dev/null
+
+			if test $status = 0
+
+				unset_project
+
+				set -Ux DEV_PROJECT $argv[1]
+
+				echo "Starting new project's containers ($DEV_PROJECT):"
+				echo ""
+				cd $DEV_HOME/$DEV_PROJECT
+				fig start
+			else 
+				echo "Invalid project name"
+			end 
+		end
 	else
 		echo "Too many arguments"
 	end
